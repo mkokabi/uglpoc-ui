8410ad6cc939bb8884e0547c721a9b8b8a7aa4ab
Mohsen <mkokabi@yahoo.com>
2021-08-08 22:21:05 +1000

    Refactoring

--- FeedCacheFunc/FeedCacheFunc.cs
+++ FeedCacheFunc/FeedCacheFunc.cs
@@ -45,45 +45,68 @@ namespace UGL.PoC
     public static async Task Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
     {
       IDatabase cache = GetDatabase();
-      
+
+      int counter = await GetPreviousCounter(cache);
+
+      Response res = await CallTrainApi(counter);
+
+      string currStateJson = GetCurrentState(res);
+
+      await StoreStateInRedis(cache, counter, res, currStateJson);
+
+      await BroadcastStatusChange();
+    }
+
+    private static async Task BroadcastStatusChange()
+    {
+      var backendAPIHttpClient = new HttpClient();
+      var backendUrl = Environment.GetEnvironmentVariable("BackendApiURL");
+      var backendMessageUrl = $"{backendUrl}/Alarm?message=alarmchange";
+      await backendAPIHttpClient.PostAsync(backendMessageUrl, null);
+    }
+
+    private static async Task StoreStateInRedis(IDatabase cache, int counter, Response res, string currStateJson)
+    {
+      await cache.StringSetAsync("CurrentState", currStateJson);
+      await cache.StringSetAsync(res.src[0].cfg_hash, currStateJson);
+      await cache.StringSetAsync("Counter", res.src[0].last_trans_id);
+      Console.WriteLine($"cfg_hash: {res.src[0].cfg_hash} starting_counter: {counter}, Last_counter:{res.src[0].last_trans_id}");
+    }
+
+    private static string GetCurrentState(Response res)
+    {
+      var currState = new Dictionary<string, int>();
+
+      res.src[0].alarm_journal.ToList().ForEach(alarm =>
+      {
+        string key = alarm.alarmid.ToString();
+        int status = alarm.status;
+        currState[key] = status;
+      });
+
+      var currStateJson = JsonSerializer.Serialize(currState);
+      return currStateJson;
+    }
+
+    private static async Task<Response> CallTrainApi(int counter)
+    {
+      var trainAPIHttpClient = new HttpClient();
+      var trainUrl = Environment.GetEnvironmentVariable("TrainUrl");
+      var trainAlarmsUrl = $"{trainUrl}/alarmsjournal?area=VIEW&prev_trans_id={counter}";
+      var response = await trainAPIHttpClient.GetStringAsync(trainAlarmsUrl);
+      var res = JsonSerializer.Deserialize<Response>(response);
+      return res;
+    }
+
+    private static async Task<int> GetPreviousCounter(IDatabase cache)
+    {
       var counter = 4238;
       if (await cache.KeyExistsAsync("Counter"))
       {
         counter = (int)(await cache.StringGetAsync("Counter"));
       }
 
-      var prevCache = await cache.StringGetAsync("PrevCache");
-      Dictionary<string, int> prevDict = null;
-      if (!prevCache.IsNull)
-      {
-        prevDict = JsonSerializer.Deserialize<Dictionary<string, int>>(prevCache);
-      }
-
-      var trainAPIHttpClient = new HttpClient();
-      var trainUrl = Environment.GetEnvironmentVariable("TrainUrl");
-      var trainAlarmsUrl = $"{trainUrl}/alarmsjournal?area=VIEW&prev_trans_id={counter}";
-      var response = await trainAPIHttpClient.GetStringAsync(trainAlarmsUrl);
-      var res = JsonSerializer.Deserialize<Response>(response);
-      
-      var currState = new Dictionary<string, int>();
-        int status = res.src[0].alarm_journal[0].status;
-
-      res.src[0].alarm_journal.ToList().ForEach(alarm => {
-        string key = alarm.alarmid.ToString();
-        int status = alarm.status;
-        currState[key] = status;
-      });
-      
-      var currStateJson = JsonSerializer.Serialize(currState);
-      await cache.StringSetAsync("CurrentState", currStateJson);
-      await cache.StringSetAsync(res.src[0].cfg_hash, currStateJson);
-      await cache.StringSetAsync("Counter", res.src[0].last_trans_id);
-      Console.WriteLine($"cfg_hash: {res.src[0].cfg_hash} starting_counter: {counter}, Last_counter:{res.src[0].last_trans_id}");
-    
-      var backendAPIHttpClient = new HttpClient();
-      var backendUrl = Environment.GetEnvironmentVariable("BackendApiURL");
-      var backendMessageUrl = $"{backendUrl}/Alarm?message=alarmchange";
-      await backendAPIHttpClient.PostAsync(backendMessageUrl, null);
+      return counter;
     }
   }
 }
